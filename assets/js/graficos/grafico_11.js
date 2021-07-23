14265be8fb6ccc500aaa0a994d3cc356ccbfe1d0
zegildo <zegildo@gmail.com>
2021-07-23 09:04:39 -0300

    melhoria do grafico 11 e 2

--- assets/js/graficos/grafico_11.js
+++ assets/js/graficos/grafico_11.js
@@ -8,34 +8,41 @@ var ctx = document.getElementById('grafico_11');
 						  {
 							label: 'Sociedade com PF',
 							data: [11882327, 10803637],
-							backgroundColor:'rgba(255, 99, 132, 0.2)' ,
-							borderColor: 'rgba(255, 99, 132)', 
+							backgroundColor:'rgba(54, 162, 235, 0.2)' ,
+							borderColor: 'rgba(54, 162, 235)', 
 							borderWidth: 1,
 						  },
 						  {
-							label: 'Sociedade com PJ',
-							data: [500487,272371],
-							backgroundColor:'rgba(54, 162, 235, 0.2)' ,
-							borderColor: 'rgb(54, 162, 235)',
-							borderWidth: 1,
-						  },
+							label: 'Diferença',
+							data: [11882327,10803637],
+							borderColor: 'rgba(255, 99, 132)',
+							type: 'line',
+							fill: false,
+							order: 0
+						  }
 						],
 						
 					  },
 				
 				
-							options: {
-								responsive: true,
-								plugins: {
-								  legend: {
-									position: 'top',
-								  },
-								  title: {
-									display: true,
-									text: 'Chart.js Floating Bar Chart'
-								  }
+					  options: {
+						scales: {
+							yAxes: [{
+								ticks: {
+									beginAtZero: true
 								}
-							  }
-				
+							}]
+						},
+						responsive: true,
+						plugins: {
+						  legend: {
+							position: 'top',
+						  },
+						  title: {
+							display: true,
+							text: 'Quantidade de Empresas 2020 vs 2021'
+						  }
+						}
+					  }
 				
 				});
--- assets/js/graficos/grafico_2.js
+++ assets/js/graficos/grafico_2.js
@@ -1,41 +1,24 @@
 var ctx = document.getElementById('grafico_2');
-				var myChart = new Chart(ctx, {
-				    type: 'doughnut',
-				    data: {
-				        labels: ['Pessoa Física', 'Pessoa Jurídica', 'Estrangeiro'],
-				        datasets: [{
-				            label: '# relações',
-				            data: [26162703, 787133, 19007],
-				            backgroundColor: [
-				                'rgba(255, 99, 132, 0.2)',
-				                'rgba(54, 162, 235, 0.2)',
-				                'rgba(11, 156, 49, 0.2)'
-				            ],
-				            borderColor: [
-				                'rgba(255, 99, 132, 1)',
-				                'rgba(54, 162, 235, 1)',
-				                'rgba(11, 156, 49, 1)'
-				            ],
-				            borderWidth: 1
-				        },
-					
-						{
-				            label: '# relações',
-				            data: [26162703, 787133, 19007],
-				            backgroundColor: [
-				                'rgba(255, 99, 132, 0.2)',
-				                'rgba(54, 162, 235, 0.2)',
-				                'rgba(11, 156, 49, 0.2)'
-				            ],
-				            borderColor: [
-				                'rgba(255, 99, 132, 1)',
-				                'rgba(54, 162, 235, 1)',
-				                'rgba(11, 156, 49, 1)'
-				            ],
-				            borderWidth: 1
-				        }]
-				    },
-				    options: {
-				        cutoutPercentage: 50
-				    }
-				});
+var myChart = new Chart(ctx, {
+	type: 'bar',
+	data: {
+		labels: ['junho 2020','maio de 2021'],
+		datasets: [
+			{
+				label: 'Sociedade com PJ',
+				data: [500487,272371],
+				backgroundColor:'rgba(54, 162, 235, 0.2)' ,
+				borderColor: 'rgb(54, 162, 235)',
+				borderWidth: 1,
+			},
+			{
+				label: 'Diferença',
+				data: [500487,272371],
+				borderColor: 'rgba(255, 99, 132)',
+				type: 'line',
+				fill: false,
+				order: 0
+			  }
+			]
+		},
+});
